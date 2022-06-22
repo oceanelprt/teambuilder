dc1d2648f53c9cfb05673c5323bdac388f5dfccc
unknown <Administrateur@25SE23-2T0TVD3.ad.campus-eni.fr>
2022-06-22 17:23:00 +0200

    add comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,7 @@ export class AppComponent implements OnInit {
     // fct exc lors de la creation du component par angular
     // apres l exec du constructor
     this.equipes = this.equipeService.equipes
-    }
+  }
 
   onAjouterPersonne(){
     this.idPersonne = this.personnes.length;
@@ -33,10 +33,13 @@ export class AppComponent implements OnInit {
     let personne = new Personne(this.prenomPersonne, this.nomPersonne, this.idPersonne);
 
     this.personnes.push(personne);
+
+    //si une équipe est choisie on ajoute la personne à l'équipe
     if (this.indiceEquipe != -1) {
       this.equipeService.ajouterPersonne(this.indiceEquipe, personne);
     }
 
+    //remise à 0 des champs
     this.nomPersonne = "";
     this.prenomPersonne = "";
     this.indiceEquipe = -1;
@@ -46,20 +49,22 @@ export class AppComponent implements OnInit {
 
     this.personnes.splice(indexPersonne, 1);
 
-
+    // itération sur les équipes
     for (let i = 0; i < this.equipes.length; i++) {
       
+      // itération sur les personnes d'une équipe
       for (let j = 0; j < this.equipes[i].personnes.length; j++) {
+        //vérification si la personne est dans l'équipe
         if (this.equipes[i].personnes[j].id == idPersonne) {
           this.equipeService.enleverPersonne(i, j);
         }
       }
     };
   }
-  
 
   onAjouterEquipe(){
     this.equipeService.ajouterEquipe(new Equipe(this.nomEquipe));
+    //remise à 0 du champ
     this.nomEquipe='';
   }
 }
